dbe3f5afd2327fc0f3490fa25f56813e4f4239b1
chentianyuan <chentianyuan2017@outlook.com>
2017-12-06 17:50:30 +0800

    react-music-player

---
+++ demo05/app/root.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header'
+
+class Root extends React.Component{
+	constructor(){
+		super()
+	}
+	render(){
+		return(
+			<Header/>
+		)
+	}
+}
+
+export default Root
---
+++ demo05/webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack')
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+	//不写./会去node_modules里面找
+	entry:[
+		//不直接写入口文件 "./app/index.js" ./可以用__dirname代替
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		'react-hot-loader/patch',
+		path.join(__dirname,"app/index.js")
+	],
+	output:{
+		path:path.join(__dirname,'/dist/'),
+		filename:"[name].js",
+		publicPath:'/'
+	},
+	//插件配置
+	plugins:[
+		new HtmlWebpackPlugin({
+			template:'./index.tpl.html',
+			inject:'body',
+			filename:'./index.html'
+		}),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV':JSON.stringify('developement')
+		})
+	],
+	//模块配置
+	module:{
+		loaders:[
+			{
+				//匹配
+				test:/\.js$/,
+				//不匹配node_modules中的.js
+				exclude:/node_modules/,
+				loader:'babel-loader',
+				query:{
+					presets:['react','es2015']
+				}
+			},
+			{
+				test:/\.css$/,
+				loader:'style!css'
+			},{
+				test:/\.less$/,
+				loader:'style-loader!css-loader!less-loader'
+			}
+		]
+	}
+}
